186326f546e32e4c9ec77885efb0e7d0957cd6c6
Jayanth Ravuri <98194817+jayr0119@users.noreply.github.com>
2022-11-11 19:08:16 -0500

    Update NETS.java

--- src/main/NETS.java
+++ src/main/NETS.java
@@ -1,5 +1,14 @@
 package src.main;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class NETS {
+    public void NetsOutlier(){}//To find the Outlier in NETS way.
+    public void NetEffect(){}// Function to compute the net change in the slides containing a group of datapoints.
     
 }
